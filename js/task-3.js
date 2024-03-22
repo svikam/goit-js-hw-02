7ab7932a5ea10754123c5777ac86e1d0a0d310be
svikam <svikam1@gmail.com>
2024-03-22 19:33:14 +0200

    hw-02 22.03.24

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,14 @@
 'use strict';
-function getElementWidth(content, padding, border) {
-    const boxSizing = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
-    return boxSizing;
+function checkForSpam(message) {
+    const messageLC = message.toLowerCase();
+    return messageLC.includes("spam") || messageLC.includes("sale");
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
